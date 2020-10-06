5443bfe3076a97b239480953908a371b5b3c8fc8
valenwin <tina.lysenok@gmail.com>
2020-10-07 00:50:16 +0300

    minor update

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'fjdsfjsdfsdvycuxvjzb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['todo-list-app.herokuapp.com']
+ALLOWED_HOSTS = ['todo-list-app-django.herokuapp.com']
 
 ROOT_APPS = [
     'core',
